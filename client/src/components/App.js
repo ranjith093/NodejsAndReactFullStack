cdac1de7240acd5a4183d9977b3ad835490c88b0
ranjithnair093 <ranjithnair093@gmail.com>
2020-08-29 12:28:22 +0530

    Survey Frontent completed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,35 +1,31 @@
-import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import { connect } from 'react-redux';
-import * as actions from '../actions';
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-import Header from './Header';
-import Landing  from './Landing';
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-
+import Header from "./Header";
+import Landing from "./Landing";
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends React.Component {
-    componentDidMount(){
-        this.props.fetchUser();
-    }
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-    render() {
-        return (
-            <div>
-                <BrowserRouter>
-                    <div className="container">
-                        <Header />
-                        <Route exact path="/" component={Landing}/>
-                        <Route exact path="/surveys" component={Dashboard} />
-                        <Route path="/surveys/new" component={SurveyNew} />
-                    </div>
-                </BrowserRouter>
-            </div>
-        )
-    } 
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    );
+  } 
 }
-
 
 export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,51 +1,56 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import Payments from './Payments'; 
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Payments from "./Payments";
 class Header extends Component {
-    renderContent(){
-        switch(this.props.auth){
-            case null:
-                return 'Loading';
-            case false:
-                return (
-                    <li><a href="/auth/google">Login With Google</a></li>
-                )
-            default:
-                return [
-                <li key="1"><Payments /></li>,
-                <li key="2" style={{margin: '0 10px' }}>
-                    Credits: {this.props.auth.credits}
-                </li>,
-                <li key="3"> <a href="/api/logout">Logout</a></li>
-                ];
-        }
-    }
-    render(){
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return "Loading";
+      case false:
         return (
-            <nav>
-                <div className="nav-wrapper">
-                    <Link to={
-                        this.props.auth ? '/surveys' : '/'
-                    } className="left brand-logo"
-                    >
-                        Emaily
-                    </Link>
-                    <ul className="right">
-                        {this.renderContent()}
-                    </ul>
-                </div>
-            </nav>
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
         );
+      default:
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="2" style={{ margin: "0 10px" }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="3">
+            {" "}
+            <a href="/api/logout">Logout</a>
+          </li>,
+        ];
     }
+  }
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
 }
 // function mapStateToProps(state){
 //     return {
-//        auth: state.auth 
+//        auth: state.auth
 //     }
 // }
-function mapStateToProps({auth}){
-    return { auth }
+function mapStateToProps({ auth }) {
+  return { auth };
 }
 
 export default connect(mapStateToProps)(Header);
